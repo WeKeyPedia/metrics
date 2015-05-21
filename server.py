555e76a62f0ec955932f95bec444e7c360f23241
taniki <tk@deveha.com>
2015-05-21 16:36:41 +0200

    use environment variable to set port

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask.ext.cors import CORS
+
 
 from api import api
 
@@ -9,5 +12,7 @@ app = Flask(__name__)
 cors = CORS(app)
 
 if __name__ == "__main__":
+  port = os.environ.setdefault("PORT", "5000")
+
   app.register_blueprint(api)
-  app.run(debug=True, port=5100)
+  app.run(debug=True, port=int(port))
